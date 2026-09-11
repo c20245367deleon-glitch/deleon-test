5621c3d6f65df918f40e78ca25375dbca34f1c53
c20245654caranay-bit <c20245654.caranay@csav.edu.ph>
2026-09-11 09:24:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World.");
+console.log("first Javascript");
+
+const myName = "Nicole De Leon";
+myNumber = 09678915863,
+    myAddress = "purok Humayan brgy luna cadiz city negros occidental";
+let age = 20;
+
+console.log("Name: " + myName);
+console.log("Number: " + myNumber);
+console.log("Address: " + myAddress);
+console.log("Age: " + age);
